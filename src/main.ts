3509febde3f1787d2e4ba9874f3e5b7fd2da3d1f
jade ! <150134796+kiitaristi@users.noreply.github.com>
2025-10-17 03:53:46 +0000

    Mouse input draws to canvas and clear button clears (step 2 complete)
    Co-authored-by: Cameron Coleman <TheSkyIsForward2@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
 import "./style.css";
 
-document.body.innerHTML = `
-  <div class="background">
-    <div style=
-      margin = 0;
-      padding = 0;
-    >
-      <h1 style="margin: 10;">Title</h1>
-    </div>
+"use strict";
 
-    <div class="divider">
-      <canvas class="myCanvas"></canvas>
-    </div>
-  </div>
-  `;
+document.body.innerHTML = `
+  <h1 style="color:black;">Title</h1>
+  <canvas id="canvas" width=256 height=256 class="canvas"></canvas>
+  <button id="clearbutton" class="clearbutton">Clear</button>
+`;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const button = document.getElementById("clearbutton")!;
+const ctx = canvas.getContext("2d");
+if (!ctx) throw new Error("Failed to get canvas context");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+button.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
