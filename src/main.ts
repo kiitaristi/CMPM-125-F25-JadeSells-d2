f99359293f1bbe688cbe3e47a72e5db8ed385d97
jade ! <kiitaristi@gmail.com>
2025-12-05 05:15:19 -0800

    Add functionality and UI implementation for multiple markers (step 6 complete)

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,25 @@ document.body.innerHTML = `
   <h1 style="color:black;">Title</h1>
   <canvas id="canvas" width=256 height=256 class="canvas"></canvas>
   <div>
-    <button id="clearbutton" class="button">Clear</button>
-    <button id="undobutton" class="button">Undo</button>
-    <button id="redobutton" class="button">Redo</button>
+    <button id="clearbutton" class="stateButton">Clear</button>
+    <button id="undobutton" class="stateButton">Undo</button>
+    <button id="redobutton" class="stateButton">Redo</button>
+  </div>
+  <div id="weightbuttons">
+    <button id="thinmarkerbutton" class="toolButton">Thin Marker</button>
+    <button id="thickmarkerbutton" class="toolButton">Thick Marker</button>
+  </div>
+  <div style="color: black; font-size: 50; margin: 10px;">Current line weight: <span id="weight">Thin Marker</span>
   </div>
 `;
 
 class Marker {
   private pointData: { x: number; y: number }[] = [];
+  private lineWeight: number;
 
-  constructor(xStart: number, yStart: number) {
+  constructor(xStart: number, yStart: number, weight: number) {
     this.pointData.push({ x: xStart, y: yStart });
+    this.lineWeight = weight;
   }
 
   draw(x: number, y: number) {
@@ -24,6 +32,8 @@ class Marker {
   }
 
   display(ctx: CanvasRenderingContext2D) {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = this.lineWeight;
     if (this.pointData.length > 1) {
       ctx.beginPath();
       const { x, y } = this.pointData[0];
@@ -40,12 +50,33 @@ const lines: Marker[] = [];
 const redoLines: Marker[] = [];
 
 let currLine: Marker | null = null;
+let currWeight: number = 2;
 
 const cursor = { active: false, x: 0, y: 0 };
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 if (!ctx) throw new Error("Failed to get canvas context");
+
+const weightDesc = document.getElementById("weight")!;
+
+const thinButton = document.getElementById("thinmarkerbutton")!;
+thinButton.classList.add("selectedTool");
+
+const thickButton = document.getElementById("thickmarkerbutton")!;
+
+function selectTool(weight: number, button: HTMLElement) {
+  currWeight = weight;
+
+  thinButton.classList.remove("selectedTool");
+  thickButton.classList.remove("selectedTool");
+  button.classList.add("selectedTool");
+
+  weightDesc.textContent = button.textContent;
+}
+
+thinButton.addEventListener("click", () => selectTool(2, thinButton));
+thickButton.addEventListener("click", () => selectTool(5, thickButton));
 
 const redrawEvent = new Event("drawing-changed");
 
@@ -54,7 +85,7 @@ canvas.addEventListener("mousedown", (e) => {
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
 
-  currLine = new Marker(cursor.x, cursor.y);
+  currLine = new Marker(cursor.x, cursor.y, currWeight);
   redoLines.splice(0, redoLines.length);
   lines.push(currLine);
 
